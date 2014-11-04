3fdb6ee54a1fb17703199e7bc243d54d1eb3086e
Paride Casulli <paride.casulli@gmail.com>
2014-11-04 11:00:05 +0100

    continuato sviluppo backend server e notification manager

--- backend_server/NotificationManager.py
+++ backend_server/NotificationManager.py
@@ -5,6 +5,11 @@ import boto.sqs
 from boto.sqs.message import Message
 import threading
 import json
+import JSONManager as jm
+import time
+
+#main code creates a thread for each quandrant managed, if the % of the
+#occupation crosses a threshold sends a notification to the right queue
 
 class NotificationManager(threading.Thread):
 	__myQuadrantID	=	0
@@ -15,12 +20,36 @@ class NotificationManager(threading.Thread):
 		threading.Thread.__init__(self)
 		self.myQuadrantID	=	anId
 		self.frequency		=	freq	
-		self.mysqsZone		=	sqsZone
+		self.mysqsZone		=	(str)sqsZone
 	
 	
 	def run(self):
-		print "Checking status notifications for the following quadrant"+str(self.myQuadrantID)
-		sleep(frequency)
+		prevStatus	=	0
+		conn = boto.sqs.connect_to_region(self.mySettings.settings[self.mysqsZone])
+		queueName	=	"_SDCC_NOTIFICATION"+str(self.quadrant.getID())
+		my_queue = conn.get_queue(queueName)
+		#print "pippo" + str(my_queue)
+		while my_queue == None:
+			#print "creating SQS queue "+queueName
+			my_queue = conn.create_queue(str(queueName))
+			if my_queue==None:
+				print "queue creation failed"
+		print my_queue
+		while True:
+			print "Checking status notifications for the following quadrant"+str(self.myQuadrantID)
+			aRequestId				=	int(time.time())
+			JsonRequest				=	jm.createOverviewRequest(aRequestId,queueName,self.myQuadrantID)
+			destinationQueueName	=	"_SDCC_"+str(self.myQuadrantID)
+			dest_queue = conn.get_queue(destinationQueueName)
+			while dest_queue == None:
+				dest_queue = conn.create_queue(str(destinationQueueName))
+				if dest_queue==None:
+					print "queue creation failed"
+			print "destination queue"+str(dest_queue)
+			m = Message()
+			m.set_body(str(JsonRequest))
+			dest_queue.write(m)
+			sleep(frequency)
 		
 settingsHandler		=	settings.Settings("testimp.txt")
 try:
--- backend_server/QuadrantHandler.py
+++ backend_server/QuadrantHandler.py
@@ -6,6 +6,8 @@ from boto.sqs.message import Message
 import threading
 import json
 import ParkingDYDBLoader as DBloader
+import JSONManager as jm
+from boto.sqs.message import Message
 
 class QuadrantHandler(threading.Thread):
 	__quadrant	=	0
@@ -32,27 +34,34 @@ class QuadrantHandler(threading.Thread):
 			#print "Working"
 			requests	=	my_queue.get_messages(wait_time_seconds=20)#tanto di default ne preleva solo 1
 			print "queue "+str(queueName)+"pulled "+str(len(requests))+" messages"
+			myResponse 	=	""
 			for item in requests:
 				print "deleting "+str(item)
 				text	=	item.get_body()
 				my_queue.delete_message(item)
 				myrequest =	json.loads(text)
-				aQuadrantID	=	myrequest[0]["quadrant"]
+				aQuadrantID		=	myrequest[0]["quadrant"]
+				requestID		=	myrequest[0]["r_id"]
+				responseQueue	=	myrequest[0]["resp_queue"]
+				myResponse		=	-1
 				if int(aQuadrantID)!=int(self.quadrant.getID()):
 					raise Exception("wrong quadrant request "+str(aQuadrantID)+" on "+str(self.quadrant.getID()))
 				self.myLoader.batchUpdate(self.quadrant.getParkList())	#update status
 				rtype	=	myrequest[0]["type"]
 				if str(rtype)=="overview":
-					print "percentuale parcheggi liberi "+str(self.quadrant.getPercentageFreeParkings())
-					print "overview request"
+					freePercentage	=	int(self.quadrant.getPercentageFreeParkings())
+					print "percentuale parcheggi liberi "+str(requestID)
+					print "Serving an overview request"
+					myResponse	=	jm.createOverviewResponse(requestID,freePercentage,self.quadrant.getID())
+					print "Overview JSON response "+str(myResponse)
 				elif str(rtype)=="full_list":
 					tempList	=	list()
 					myParkList	=	self.quadrant.getParkList()
 					for item in myParkList:
-						tempList.append(item.getDictionary())
-					jsonString	=	json.dumps(tempList)
+						tempList.append(item.getDictionary()) #creates a list of dictionary, each item in dictionary is a parking
 					print "full list request"
-					print jsonString
+					myResponse	=	jm.createListResponse(requestID,tempList)
+					print "full list JSON response"+str(myResponse)
 				elif str(rtype)=="bounded_list":
 					tempList	=	list()
 					maxlat		=	float(myrequest[0]["NWlat"])
@@ -65,11 +74,25 @@ class QuadrantHandler(threading.Thread):
 						itemLon	=	float(item.getLongitude())
 						if((itemLat<=maxlat)and(itemLat>=minlat)and(itemLon>=minlon)and(itemLon<=maxlon)):
 							tempList.append(item.getDictionary())
-					jsonString	=	json.dumps(tempList)
-					print jsonString
 					print "bounded list request"
+					myResponse	=	jm.createListResponse(requestID,tempList)
+					print "bounded list JSON response"+str(myResponse)
 				else:
 					raise Exception("Unknown type request")
+				#CODICE DA TESTARE!!!
+				if myResponse not -1:
+					resp_queue = conn.get_queue(str(responseQueue))
+					print "Response queue" + str(my_queue)
+					while my_resp_queue == None:
+						#print "creating SQS queue "+queueName
+						my_resp_queue = conn.create_queue(str(responseQueue))
+					if my_resp_queue==None:
+						print "queue creation failed"
+					m = Message()
+					m.set_body(str(myResponse))
+					my_resp_queue.write(m)
+				else:
+					print "QuadrantHandler.py: error on request processing"
 				
 
 
