27812af7305ea6ae3fa476ad0f0f85cf79edb991
Paride Casulli <paride.casulli@gmail.com>
2014-12-03 21:35:03 +0100

    test

--- backend_server/Quadrant.py
+++ backend_server/Quadrant.py
@@ -65,6 +65,7 @@ class Quadrant:
 		else:
 			free	=	0
 			#print "Quadrant.py number of parkings in quadrant "+str(self.qid)+" = "+str(self.getNumberOfParkings())
+			print "Quadrant.py CACHE MISS"
 			parkingList	=	self.getParkList() 
 			self.updater.batchUpdate(parkingList)
 			for item in parkingList:
