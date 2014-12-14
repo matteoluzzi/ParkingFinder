b50269e774c466b78e765a3434565c2f3b83b9ea
Matteo Luzzi <matteo.luzzi@gmail.com>
2014-12-14 19:58:35 +0000

    Scheletro per registrazione utente al sito (TODO risolvere bug refresh pagina)

--- front_end_server.py
+++ front_end_server.py
@@ -2,6 +2,7 @@
 # coding=utf-8
 
 import os, sys, time, json, itertools, ConfigParser
+from bcrypt import hashpw, gensalt
 
 from boto.sqs.message import Message
 import boto.sqs as sqs
@@ -18,6 +19,7 @@ import tornado.options
 import tornado.web
 import tornado.gen
 import tornado.websocket
+from tornado.escape import xhtml_escape, xhtml_unescape
 
 class BaseHandler(tornado.web.RequestHandler):
 
@@ -27,7 +29,50 @@ class BaseHandler(tornado.web.RequestHandler):
 		self._port = port
 	
 	def get(self):
-		self.render("map.html", feAddress=self._address, fePort=self._port)
+		self.render("map.html", feAddress=self._address, fePort=self._port, signup=None)
+
+class RegisterHandler(tornado.web.RequestHandler):
+
+	def initialize(self, front_end_address, port):
+
+		self._address = front_end_address
+		self._port = port
+
+	@tornado.web.asynchronous
+	def post(self):
+
+		username = xhtml_escape(self.get_argument("username"))
+		email = xhtml_escape(self.get_argument("email"))
+		password = xhtml_escape(self.get_argument("password"))
+		conf_pass = xhtml_escape(self.get_argument("confirmPassword"))
+
+		#Thread incaricato di gesitire la scrittura sul db
+		pool = ThreadPool(processes=1)
+
+	
+		pool.apply_async(self.__checkDuplicates, args=(username, email, password), callback=self.__onfinish)
+
+	def __checkDuplicates(self, username, email, password):
+
+		signup = "Utente registrato con successo! Effettuare il login per accedere"
+
+		#manca controllo su database del nome utente e password
+		#se entrambi i controlli vanno a buon fine allora posso registrare l'utente sul db
+		#setta error se c'è un errore
+
+		encrypted_password = hashpw(password, gensalt())
+
+		#salva le info sul db
+
+		return (encrypted_password, signup)
+
+
+	def __onfinish(self, args):
+
+		print "entrypted password---------->" + args[0]
+		self.render("map.html", feAddress=self._address, fePort=self._port, signup=args[1]) #nella render è presente self.finish()
+		#self.redirect("/")
+
 		
 class MapHandler(tornado.websocket.WebSocketHandler):
 
@@ -65,12 +110,8 @@ class MapHandler(tornado.websocket.WebSocketHandler):
 		q_ids = set(map(lambda x: int(x),quadrants.split("|")))
 		print q_ids
 
-		#ad ogni richiesta instanzio un pool di thread per gestire le chiamate ad sqs
-		#pool = ThreadPool(100)
-
 		re_write = yield tornado.gen.Task(self.__write_background, self.__send_parking_spots_request, args=(idReq, zoom_level, q_ids, neLat, neLon, swLat, swLon), kwargs={'pool':self._pool, 'settings':self._settings})
 
-		#pool.close()
 
 	def on_close(self):
 		print "WebSocket closed!"
@@ -112,27 +153,18 @@ class MapHandler(tornado.websocket.WebSocketHandler):
 
 		for id in sqs_queues_ids:
 			try:
-	#			if id in range(1,21):
-
 				request = True
 				pool.apply_async(func, args=(self._sqs_request_queue, id ,len(sqs_queues_ids), args[0], args[1], args[3], args[4], args[5], args[6], settings))					
 
 			except KeyError:
-				self.write_message(json.dumps({"type": "not found",  "quadrantID" : id, "percentage": -1}))
-
-		if request == False:
-			tornado.ioloop.IOLoop.instance().add_callback(lambda: callback(json.dumps("{res : -1}")))
-			
-				
-	
+				self.write_message(json.dumps({"type": "not found",  "quadrantID" : id, "percentage": -1}))			
 		
 	def __send_parking_spots_request(self, queue, q_id, req_size, idReq, zoom_level, neLat, neLon, swLat, swLon, settings):
-		 			
-	#	print "nella sending ...."
 
-		if int(zoom_level) >= 20:
-			req_type = settings['specific_req_type'] + zoom_level
-			data = self.__create_request_message(settings, idReq, req_type, q_id, zoom_level, neLat, neLon, swLat, swLon)
+		if int(zoom_level) >= 18:
+			req_type = settings['specific_req_type']
+			print req_type
+			data = self.__create_request_message(settings, idReq, req_type, q_id, zoom_level=zoom_level, neLat=neLat, neLon=neLon, swLat=swLat, swLon=swLon)
 		else:
 			req_type = settings['global_req_type']
 			data = self.__create_request_message(settings, idReq, req_type, q_id , zoom_level=zoom_level)
@@ -142,7 +174,7 @@ class MapHandler(tornado.websocket.WebSocketHandler):
 		msg = Message()
 		msg.set_body(data)	
 		res = queue.write(msg)
-	#	print "scritto messaggio ", res.get_body()
+		print "scritto messaggio ", res.get_body()
 		
 		queue = self._dispatcher.get_message_queue(idReq)
 
@@ -158,11 +190,6 @@ class MapHandler(tornado.websocket.WebSocketHandler):
 			tornado.ioloop.IOLoop.instance().add_callback()			
 		else:
 			self.write_message(message)
-
-
-#		print "messaggio scritto al client"
-	
-
 
 	@staticmethod
 	def __create_request_message(settings, idReq, reqType, q_id, zoom_level=None, neLat=None, neLon=None, swLat=None, swLon=None):
@@ -201,7 +228,10 @@ def initialize(sqs_conn, settings):
 	pool = ThreadPool(int(settings['pool_size']))
 
 		
-	app = tornado.web.Application([(r'/', BaseHandler, dict(front_end_address=settings['front_end_address'], port=settings['port'])), (r'/map', MapHandler, dict(sqs_conn=sqs, request_queue=request_queue, dispatcher=dispatcher, quadrantslist=quadrantslist, pool=pool, settings=settings))], template_path=os.path.join(os.path.dirname(__file__), "templates"), static_path=os.path.join(os.path.dirname(__file__), "static"), debug = True,)
+	app = tornado.web.Application([(r'/', BaseHandler, dict(front_end_address=settings['front_end_address'], port=settings['port'])), 
+		(r'/map', MapHandler, dict(sqs_conn=sqs, request_queue=request_queue, dispatcher=dispatcher, quadrantslist=quadrantslist, pool=pool, settings=settings)),
+		(r'/register', RegisterHandler, dict(front_end_address=settings['front_end_address'], port=settings['port']))], 
+		template_path=os.path.join(os.path.dirname(__file__), "templates"), static_path=os.path.join(os.path.dirname(__file__), "static"), debug = True,)
 	http_server = tornado.httpserver.HTTPServer(app)
 	http_server.listen(settings['port'])
 	print "ready to serve"
